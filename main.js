37f3309aef7cad75c491bc0b9d7ea3d1260217a6
Huynh Minh Hoang <hoanghuynh0609@gmail.com>
2020-08-07 17:08:57 +0700

    update header

--- main.js
+++ main.js
@@ -11,14 +11,32 @@ function lightMode() {
 // DOM Event
 var modal = document.getElementById("myModal");
 var btn = document.getElementById("myBtn");
+var submitBtn = document.getElementById("submitBtn");
+var submitModal = document.getElementById("overlaySubmit");
+var bell = document.querySelector(".header__notification");
+var bellElement = document.getElementById("bellModal");
+var bellNoti = document.querySelector(".header__notification-noti");
 
 btn.onclick = function () {
   modal.style.display = "block";
 };
+submitBtn.onclick = function () {
+  submitModal.style.display = "flex";
+};
 
-window.onclick = function (event) {
-  if (event.target == modal) {
+bell.onclick = function () {
+  bellElement.style.display = "block";
+  bellNoti.style.display = "none";
+};
+window.onclick = function (e) {
+  if (e.target == modal) {
     modal.style.display = "none";
+  }
+  if (e.target == submitModal) {
+    submitModal.style.display = "none";
+  }
+  if (e.target == bellElement) {
+    bellElement.style.display = "none";
   }
 };
 
